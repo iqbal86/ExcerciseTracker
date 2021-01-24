64a8902f9204af8ab74760454836e401010cc2eb
Iqbal <iqbal86@live.com>
2021-01-24 23:15:54 +0500

    MileStone 3: Plus some changes in Milestone 2 to add request for permission dialog.

--- app/src/main/java/com/example/excercisetracker/MainActivity.java
+++ app/src/main/java/com/example/excercisetracker/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.excercisetracker;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -49,42 +48,40 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recordButton = (Button) findViewById(R.id.RecordButton);
-
-        recordButton.setOnClickListener(new View.OnClickListener() {
-//            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View v) {
-                if(!recordmode){
-                    recordButton.setText("Stop Recording");
-                    recordmode=true;
-                    toastMessage("Recording Started!");
-                    startrecordinglocation();
-
-                }else{
-                    recordButton.setText("Start Recording");
-                    recordmode=false;
-                    toastMessage("Recording Finished!");
-                    Intent intent = new Intent(MainActivity.this, ReportActivity.class);
-                    intent.putExtra("filename", currentfilename);
-                    startActivity(intent);
-
-                }
-
+        recordButton = findViewById(R.id.RecordButton);
+        recordButton.setOnClickListener(v -> {
+            if (needPermission()) {
+                requestPermissions();
+            } else {
+                onStartRecording();
             }
         });
-
     }
-    private void toastMessage(String message){
-        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+
+    private void onStartRecording() {
+        if (!recordmode) {
+            recordButton.setText("Stop Recording");
+            recordmode = true;
+            toastMessage("Recording Started!");
+            startrecordinglocation();
+        } else {
+            recordButton.setText("Start Recording");
+            recordmode = false;
+            toastMessage("Recording Finished, Please wait generating report!");
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        mylastlocation=location;
+        mylastlocation = location;
 
     }
-    public void startrecordinglocation(){
+
+    public void startrecordinglocation() {
 
         // create folder in external storage if doesnt exist
         String root = Environment.getExternalStorageDirectory().toString();
@@ -96,66 +93,58 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 } else {
                     toastMessage("Unable to create Folder");
                 }
-            }catch(Exception e){
+            } catch (Exception e) {
                 toastMessage(e.getMessage());
             }
         }
 
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-        String filename= df.format(new Date())+".gpx";
-        currentfilename=filename; // storing the variable in global to pass on to next activity
-        File file = new File (myDir, filename);
+        String filename = df.format(new Date()) + ".gpx";
+        currentfilename = filename; // storing the variable in global to pass on to next activity
+        File file = new File(myDir, filename);
 
         try {
             FileWriter writer = new FileWriter(file, false);
             writer.append(getfileheaders(filename));
-            writeCoordinatestoFile(writer);
+            initLocationService(writer);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             toastMessage("Error in file writing");
-            Log.e(TAG, "Error Writting Path",e);
-           
+            Log.e(TAG, "Error Writting Path", e);
         }
 
 
     }
-    public String getfileheaders(String filename){
+
+    public String getfileheaders(String filename) {
         String header = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?><gpx xmlns=\"http://www.topografix.com/GPX/1/1\" creator=\"MapSource 6.15.5\" version=\"1.1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"  xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\"><trk>\n";
         String name = "<name>" + filename + "</name><trkseg>\n";
-    return header+name;
+        return header + name;
     }
-    public String getfilefooter(){
+
+    public String getfilefooter() {
         String footer = "</trkseg></trk></gpx>";
-    return footer;
+        return footer;
     }
-    public void writeCoordinatestoFile(FileWriter writer){
+
+    public void initLocationService(FileWriter writer) {
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
-
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                if(mylastlocation!=null){
-                    if(recordmode==true) {
+                if (mylastlocation != null) {
+                    if (recordmode == true) {
                         runOnUiThread(new Runnable() {
-
                             @Override
                             public void run() {
                                 DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
                                 toastMessage("Lat: " + mylastlocation.getLatitude() + " , Lon : " + mylastlocation.getLongitude() + " Alt: " + mylastlocation.getAltitude());
-                               Location l=mylastlocation;
-                                String segment= "<trkpt lat=\"" + l.getLatitude() + "\" lon=\"" + l.getLongitude() + "\"><ele>"+mylastlocation.getAltitude()+"</ele><time>" + df.format(new Date(l.getTime())) + "</time></trkpt>\n";
+                                Location l = mylastlocation;
+                                String segment = "<trkpt lat=\"" + l.getLatitude() + "\" lon=\"" + l.getLongitude() + "\"><ele>" + mylastlocation.getAltitude() + "</ele><time>" + df.format(new Date(l.getTime())) + "</time></trkpt>\n";
                                 try {
                                     writer.append(segment);
                                 } catch (IOException e) {
@@ -164,23 +153,66 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
                             }
                         });
-                    }else{
-                        this.cancel();
+                    } else {
                         try {
                             writer.append(getfilefooter());
                             writer.flush();
                             writer.close();
-
+                            showReport();
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-
-
+                        this.cancel();
                     }
-
                 }
             }
         }, 0, 5000);
+
+    }
+
+    private boolean needPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (needPermission()) {
+            new AlertDialog.Builder(context)
+                    .setTitle("Permission Required!")
+                    .setMessage("The app needs Location and Storage permissions to run peroperly.")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            requestPermissions();
+                            dialog.cancel();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+    }
+
+    private void showReport() {
+        runOnUiThread(() -> {
+            Intent intent = new Intent(MainActivity.this, ReportActivity.class);
+            intent.putExtra("filename", currentfilename);
+            startActivity(intent);
+            finish();
+        });
     }
 
     @Override
--- app/src/main/java/com/example/excercisetracker/ReportActivity.java
+++ app/src/main/java/com/example/excercisetracker/ReportActivity.java
@@ -2,13 +2,193 @@ package com.example.excercisetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.util.Xml;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.RoundingMode;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class ReportActivity extends AppCompatActivity {
+    String currentfilename;
+    List<Location> userlocations;
+    float totaldistance;
+    Long totaltime;
+    float averagespeed;
+    float timeinseconds;
+    Double minalt, maxalt;
+    private TextView timeTaken, distanceCovered, maxAlt, minAlt, averageSpeed;
+    private static final SimpleDateFormat gpxDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        currentfilename = getIntent().getStringExtra("filename");
+
+        timeTaken = this.<TextView>findViewById(R.id.total_time);
+        distanceCovered = this.<TextView>findViewById(R.id.total_distance);
+        averageSpeed = this.<TextView>findViewById(R.id.average_speed);
+        maxAlt = this.<TextView>findViewById(R.id.max_altitude);
+        minAlt = this.<TextView>findViewById(R.id.min_altitude);
+
+        toastMessage(currentfilename.toString());
+        String path = Environment.getExternalStorageDirectory().toString() + "/GPStracks/" + currentfilename;
+        //String path = Environment.getExternalStorageDirectory().toString() + "/GPStracks/demo.gpx";
+
+        File gpxFile = new File(path);
+
+        new Thread(() -> {
+            try {
+                userlocations = readGpxFile(gpxFile);
+                this.runOnUiThread(() -> {
+                    if (userlocations.size() > 0) {
+                        calculatevalues();
+                        updateview();
+                    } else {
+                        toastMessage("No data available in file");
+                    }
+                });
+            } catch (SAXParseException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+    }
+
+    private void calculatevalues() {
+        totaldistance = 0;
+        for (int i = 0; i < userlocations.size() - 2; i++) {
+            totaldistance = totaldistance + userlocations.get(i).distanceTo(userlocations.get(i + 1));
+        }
+        totaltime = userlocations.get(userlocations.size() - 1).getTime() - userlocations.get(0).getTime();
+        timeinseconds = totaltime / 1000;
+        averagespeed = totaldistance / timeinseconds;
+        minalt = userlocations.get(0).getAltitude();
+        maxalt = 0.00;
+        for (int i = 0; i < userlocations.size() - 1; i++) {
+            if (userlocations.get(i).getAltitude() < minalt) {
+                minalt = userlocations.get(i).getAltitude();
+            }
+            if (userlocations.get(i).getAltitude() > maxalt) {
+                maxalt = userlocations.get(i).getAltitude();
+            }
+        }
+    }
+
+    private void updateview() {
+        DecimalFormat df = new DecimalFormat("#.####");
+        df.setRoundingMode(RoundingMode.CEILING);
+        timeTaken.setText(String.valueOf(timeinseconds) + " secs");
+        distanceCovered.setText(String.valueOf(totaldistance) + " meters");
+        averageSpeed.setText(String.valueOf(df.format(averagespeed)) + " m/s");
+        minAlt.setText(df.format(minalt).toString());
+        maxAlt.setText(df.format(maxalt).toString());
+    }
+
+    private List<Location> readGpxFile(File file) throws SAXParseException {
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+        List<Location> list = new ArrayList<Location>();
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            FileInputStream fileInputStream = new FileInputStream(file);
+            Document document = documentBuilder.parse(fileInputStream);
+            Element elementRoot = document.getDocumentElement();
+            NodeList nodelist_trkpt = elementRoot.getElementsByTagName("trkpt");
+            for (int i = 0; i < nodelist_trkpt.getLength(); i++) {
+                Node node = nodelist_trkpt.item(i);
+                NamedNodeMap attributes = node.getAttributes();
+                String newLatitude = attributes.getNamedItem("lat").getTextContent();
+                Double newLatitude_double = Double.parseDouble(newLatitude);
+                String newLongitude = attributes.getNamedItem("lon").getTextContent();
+                Double newLongitude_double = Double.parseDouble(newLongitude);
+                String newLocationName = newLatitude + ":" + newLongitude;
+                Location newLocation = new Location(newLocationName);
+                newLocation.setLatitude(newLatitude_double);
+                newLocation.setLongitude(newLongitude_double);
+                NodeList nList = node.getChildNodes();
+                for (int j = 0; j < nList.getLength(); j++) {
+                    Node el = nList.item(j);
+                    if (el.getNodeName().equals("ele")) {
+                        newLocation.setAltitude(Double.parseDouble(el.getTextContent()));
+
+                    } else if (el.getNodeName().equals("time")) {
+
+                        try {
+                            Date d = sdf.parse(el.getTextContent());
+                            newLocation.setTime(d.getTime());
+                        } catch (ParseException ex) {
+                            Log.v("Exception", ex.getLocalizedMessage());
+                        }
+
+
+                    }
+                }
+
+                list.add(newLocation);
+
+            }
+
+            fileInputStream.close();
+
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SAXException e) {
+
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            toastMessage("Error in reading file");
+        }
+
+        return list;
+    }
+
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
